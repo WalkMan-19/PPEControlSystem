a8aa35e9a71ed77fac49aa7a1300f54b45014372
Nadir <yusipov.nadir@gmail.com>
2026-05-04 21:02:20 +0300

    init

--- test_detector.py
+++ test_detector.py
@@ -2,6 +2,7 @@ from app.detection.singleton import init_detector
 import cv2
 
 detector = init_detector()
-img = cv2.imread("image3.jpg")
+
+img = cv2.imread("test/images/image5.jpg")
 results = detector.detect(img)
 print(results)
