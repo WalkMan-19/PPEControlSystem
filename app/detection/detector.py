f3595d9412e2a985459c7faef026a05fc0d3c425
Nadir <yusipov.nadir@gmail.com>
2026-05-04 21:42:53 +0300

    Added confidence params to classes

--- app/detection/detector.py
+++ app/detection/detector.py
@@ -8,6 +8,8 @@ class TorchDetector:
         self.model = YOLO(model_path)
         self.class_names = list(self.model.names.values())
         self.model.to(self.device)
+        self.conf_default = 0.25
+        self.conf_no_class = 0.75
 
     def detect(self, frame: np.ndarray) -> list:
         results = self.model(frame, verbose=False)[0]
@@ -16,7 +18,9 @@ class TorchDetector:
             x1, y1, x2, y2 = box.xyxy[0].tolist()
             conf = float(box.conf[0])
             cls_id = int(box.cls[0])
-            if conf < 0.25:
+            cls_name = self.model.names[cls_id]
+            threshold = self.conf_no_class if cls_name.startswith("NO-") else self.conf_default
+            if conf < threshold:
                 continue
             detections.append({
                 "bbox": [int(x1), int(y1), int(x2), int(y2)],
